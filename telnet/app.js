76e78737d26f4cdddcab202fc153107d6bc392eb
Andy Lu <andy15lu@gmail.com>
2023-10-08 17:24:00 +0300

    telnet/app.js changed

--- telnet/app.js
+++ telnet/app.js
@@ -7,28 +7,28 @@ const setAddress = serverURL + "/units/update";
 let run = async ()=>{
     let response = await fetch(getAddress);
     if (response.ok) { // если HTTP-статус в диапазоне 200-299
-    // получаем тело ответа (см. про этот метод ниже)
-    let json = await response.json();
-    let units = json.data;
-    for(let unit of units){
-        console.log(unit.name);
-        let values = [];
-        for(let item of unit.items){
-            console.log( item.name + ' > ' + item.meta[0].param );
-            // -- опрос по телнет и получение значения
+        // получаем тело ответа (см. про этот метод ниже)
+        let json = await response.json();
+        let units = json.data;
+        for(let unit of units){
+            console.log(unit.name);
+            let values = [];
+            for(let item of unit.items){
+                console.log( item.name + ' > ' + item.meta[0].param );
+                // -- опрос по телнет и получение значения
 
-            let value = {};
-            value[item._id] = "test";
-            values.push(value);
-        }
-        await fetch( setAddress, {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json;charset=utf-8' },
-                body: JSON.stringify(values),
-            });
+                let value = {};
+                value[item._id] = "test";
+                values.push(value);
+            }
+            await fetch( setAddress, {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json;charset=utf-8' },
+                    body: JSON.stringify(values),
+                });
         }
     } else {
-    console.log("Error");
+        console.log("Error");
     }
 }
 
